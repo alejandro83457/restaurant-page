f358f3c2cde1123c9174836834dc300aa4e96159
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-15 22:23:30 -0800

    Add footer for home page

--- src/homePage.js
+++ src/homePage.js
@@ -3,10 +3,10 @@ import myImg from "./main-img.jpg";
 // Function creates the whole home page.
 export default function getHomePage() {
   let div = document.createElement("div");
-  let footer = document.createElement("footer");
 
   let header = getHeader();
   let main = getMain();
+  let footer = getFooter();
 
   div.appendChild(header);
   div.appendChild(main);
@@ -79,4 +79,31 @@ function getMain() {
   main.appendChild(text2);
   main.appendChild(text3);
   return main;
+}
+
+function getFooter() {
+  let footer = document.createElement("footer");
+  let imgCreds = document.createElement("div");
+  let myCreds = document.createElement("div");
+  let myLink = document.createElement("a");
+  let imgLink = document.createElement("a");
+  let unsplashLink = document.createElement("a");
+
+  myLink.textContent = "alejandro83457";
+  myLink.href = "https://github.com/alejandro83457";
+  myCreds.textContent = "Made by ";
+  myCreds.appendChild(myLink);
+
+  imgLink.textContent = "Jigar Panchal";
+  imgLink.href =
+    "https://unsplash.com/@brave4_heart?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash";
+  unsplashLink.textContent = "Unsplash";
+  unsplashLink.href =
+    "https://unsplash.com/photos/a-black-and-red-background-with-wavy-lines-iV8qibFoGMQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash";
+  imgCreds.textContent = "Photo by ";
+  imgCreds.appendChild(imgLink);
+
+  footer.appendChild(imgCreds);
+  footer.appendChild(myCreds);
+  return footer;
 }
